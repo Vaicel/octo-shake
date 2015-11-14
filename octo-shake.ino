ed71f615986a3261c67b7af54e4e8b91f215b0a8
Vaicel <sarumanlol34@gmail.com>
2015-11-14 21:52:32 +0600

    Массив строк, из строки получаемой из GPS модуля.

--- octo-shake.ino
+++ octo-shake.ino
@@ -1,14 +1,17 @@
 #include <SD.h>
 #include <Wire.h>
-#include "MMA7660.h"
+//#include "MMA7660.h"
 #include <SoftwareSerial.h>
-MMA7660 acc;
+#define COUNT_OF_LATITUDE 3
+#define COUNT_OF_LONGITUDE 5
+#define COUNT_OF_TIME  1
+//MMA7660 acc;
 
 SoftwareSerial ss(3,2);
 
 void setup()
 {
-    acc.init();
+//    acc.init();
     pinMode(13, OUTPUT);
     Serial.begin(38400);
     ss.begin(38400);
@@ -22,18 +25,39 @@ void loop()
     char index = 0;
     char temp = 0;
     String dataString = "";
+    String StringMass[12];
+    int count=0;
+
     
-    acc.getAcceleration(&ax,&ay,&az);
+//    acc.getAcceleration(&ax,&ay,&az);
     while(ss.available())
     {    
         temp = ss.read();
-     dataString += String(temp);
-     index++;
-      if(index>600){
+        if(temp==','){
+            StringMass[count]=dataString;
+            temp=0;
+            dataString="";
+            count++;   
+        }
+        if(StringMass[2]=="V"){
+            for(int i=0; i<13;i++){
+                StringMass[i]="-";
+            }
+            break;
+        }
+        dataString += String(temp);
+        index++;
+        if(index>600){
         break;
       }
     }
-    if(dataString!="") Serial.println(dataString);
+    Serial.println("Longitude: ");
+    Serial.println(StringMass[COUNT_OF_LONGITUDE]);
+    Serial.println("Latitude: ");
+    Serial.println(StringMass[COUNT_OF_LATITUDE]);
+    Serial.println("Time: ");
+    Serial.println(StringMass[COUNT_OF_TIME]);
+    
    /* Serial.print("accleration: ");
     Serial.print(ax);
     Serial.print(ay);

82e08c6af8ea09e6554d651ede679f553cfaafee
dubkov <dubkov@skbrii.ru>
2015-11-17 00:36:18 +0600

    maps

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 My awesome npk project
+=====
+
+
+
+Yandex Maps API: Линии и многоугольники
+
+https://tech.yandex.ru/maps/doc/staticapi/1.x/dg/concepts/polylines-docpage/
---
+++ openmap.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import webbrowser
+new = 2 
+url = "https://maps.yandex.ru/65/novosibirsk/?um=constructor%3ANFYjq7dYUudZ-8FLs4r2ruBUlyTtG68I&ll=82.908207%2C55.060408&z=11"
+webbrowser.open(url,new=new)
+
+
+
